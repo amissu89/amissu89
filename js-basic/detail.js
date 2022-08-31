174358d825be51486d332aab79d3f08eb51106bb
amissu89 <yonglimlee@gmail.com>
2022-08-31 16:57:59 +0900

    select 까지 진행함

--- js-basic/detail.js
+++ js-basic/detail.js
@@ -31,3 +31,52 @@ document.querySelector('.list').addEventListener('click', function (e) {
     document.querySelectorAll('.tab-content')[idx].classList.add('show')
 
 })
+
+let car = {
+    name : '소나타',
+    price : 10000
+}
+
+let nodes = document.querySelectorAll('span')
+nodes[0].textContent = car.name
+nodes[1].textContent = car.price
+
+var pants = [28, 30, 32]
+var shirts = [95, 100, 105] 
+
+let selector = document.querySelector('select')
+let options = document.querySelectorAll('select')[1]
+selector.addEventListener('change', function(){
+    console.log(selector.value)
+    if(selector.value === '셔츠'){
+        options.classList.remove('hide')
+    }
+    else if(selector.value === '바지'){
+        //let pantsOption = '<option> 28 </option> <option> 30 </option>'
+        //options.innerHTML = pantsOption        
+        options.classList.remove('hide')
+        options.innerHTML = ''
+
+        pants.forEach(element => {
+            let opt = document.createElement('option')
+            opt.textContent = element
+            options.append(opt)
+        })
+    }
+    else{
+        options.classList.add('hide')
+    }
+})
+
+let p = document.createElement('p')
+p.innerHTML = 'p tag'
+document.querySelector('#test').appendChild(p)
+
+let template = '<p> 안녕 </p>'
+document.querySelector('#test').insertAdjacentHTML('beforeend', template)
+
+
+let obj = { name : 'lee', age : 20}
+for (var key in obj){
+    console.log(`key : ${key}, val : ${obj[key]}`)
+}
