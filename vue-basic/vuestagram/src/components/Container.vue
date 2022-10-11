b080b4b1170fcf448c07f3594d644fed8ff6c612
amissu89 <yonglimlee@gmail.com>
2022-10-12 08:51:45 +0900

    vuestagram posting data 만들기

---
+++ vue-basic/vuestagram/src/components/Container.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Post :posting="posting" v-for="(posting, idx) in postings" :key="idx" />
+  </div>
+</template>
+
+<script>
+import Post from "./Post.vue";
+
+export default {
+  name: "post",
+  components: {
+    Post: Post,
+  },
+  props: {
+    postings: Array,
+  },
+};
+</script>
+
+<style>
+</style>
