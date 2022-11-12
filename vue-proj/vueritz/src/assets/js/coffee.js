e9ea7d6258601b57d2163b792880dbd26be77796
amissu89 <yonglimlee@gmail.com>
2022-11-12 16:07:22 +0900

    싱글오리진 데이터 생성 완료, 팀멤버 표출 완료.

---
+++ vue-proj/vueritz/src/assets/js/coffee.js
@@ -0,0 +1,93 @@
+export default [
+    {
+        title: "[프릳츠] 디카페인 Decaffeinated 200g",
+        price: 16000,
+        category: "single",
+        description: [
+            "국가 : 콜롬비아 Colombia",
+            "지역 : 우일라 Huila",
+            "농장 : Coffee Producers from Huila",
+            "가공방식 : 디카페인 Decaffeinated",
+        ],
+        description2: [
+            "Flavor Notes : 초콜릿, 건무화과, 호두, 당밀의 긴 여운",
+            "Chocolate, Dried fig, Walnut, Molasses notes finish",
+        ],
+        image: "/img/coffee/decaffein.jpeg",
+        imageDescription: "/img/coffee/decaffein-description.jpg",
+    },
+    {
+        title: "[프릳츠] La Cruz de Tarrazú Caturra-Catuaí Black Honey",
+        price: 16000,
+        category: "single",
+        description: [
+            "국가: 코스타리카 Costa Rica",
+            "지역: 타라수 Tarrazú",
+            "농장: 라 크루스 데 타라수 La Cruz de Tarrazú",
+            "농부: 에스테반 사모라 Esteban Zamora",
+            "품종: 카투라-카투아이 Caturra-Catuaí",
+            "가공방식: 블랙 허니 Black Honey (El Cedro)",
+        ],
+        description2: [
+            "Flavor Notes : 블랙 체리, 건과일, 파파야, 캬라멜 여운이 긴 커피",
+            "Black cherry, Dreid fruit, Papaya, Caramel notes finish",
+        ],
+        image: "/img/coffee/BlackHoney.jpg",
+        imageDescription: "/img/coffee/BlackHoneye-description.jpg",
+    },
+    {
+        title: "[프릳츠] Harley S795 Washed",
+        price: 16000,
+        category: "single",
+        description: [
+            "국가: 인도 India",
+            "농장/조합: 할리 Harley",
+            "품종: 셀렉션 795 Selection 795",
+            "가공방식: 워시드 Washed",
+        ],
+        description2: [
+            "Flavor Notes : 건무화과, 브라운 슈가, 멜론, 호두의 여운",
+            "Dried fig, Brown sugar, Melon, Walnut notes finish",
+        ],
+        image: "/img/coffee/HarleyWashed.jpg",
+        imageDescription: "/img/coffee/HarleyWashed-description.jpg",
+    },
+    {
+        title: "[프릳츠] Aurora Java Washed",
+        price: 18000,
+        category: "single",
+        description: [
+            "국가: 니카라과 Nicaragua",
+            "지역: 마타갈파 Matagalpa",
+            "농장: 아우로라 Aurora",
+            "생산자: 엔리케 페루피노 Enrique Ferrufino",
+            "품종: 자바 Java",
+            "가공방식: 워시드 Washed",
+        ],
+        description2: [
+            "Flavor Notes: 루이보스 티, 파파야, 살구, 마카다미아넛의 여운",
+            "Rooibos tea, Papaya, Apricot, Macadamianut notes finish",
+        ],
+        image: "/img/coffee/JavaWashed.jpg",
+        imageDescription: "/img/coffee/JavaWashed-description.jpg",
+    },
+    {
+        title: "[프릳츠] Aurora Venecia Natural",
+        price: 22000,
+        category: "single",
+        description: [
+            "국가: 니카라과 Nicaragua",
+            "지역: 마타갈파 Matagalpa",
+            "농장: 아우로라 Aurora",
+            "생산자: 엔리케 페루피노 Enrique Ferrufino",
+            "품종: 베네시아 Venecia",
+            "가공방식: 내추럴 Natural",
+        ],
+        description2: [
+            "Flavor Notes: 라즈 베리, 바나나, 참외, 초콜릿",
+            "Rasp berry, Banana, Korean melon, Chocolate",
+        ],
+        image: "/img/coffee/Venecia.jpg",
+        imageDescription: "/img/coffee/Venecia-description.jpg",
+    }
+]
--- vue-proj/vueritz/src/components/sub-menu/TeamMember.vue
+++ vue-proj/vueritz/src/components/sub-menu/TeamMember.vue
@@ -1,8 +1,18 @@
 <template>
   <h1>Team Member</h1>
+
+  <div class="black-bg" v-if="modalShow">
+    <div class="white-bg" @click="closeModal">
+      <img :src="`${Members[selectedIdx].profile}`" alt="profile" />
+      <p>{{ Members[selectedIdx].role }} | {{ Members[selectedIdx].email }}</p>
+      <p>
+        {{ Members[selectedIdx].like }}
+      </p>
+    </div>
+  </div>
   <div class="post">
     <div class="post-content" v-for="(member, idx) in Members" :key="idx">
-      <img :src="`${member.profile}`" alt="">
+      <img :src="`${member.profile}`" alt="" @click="openProfile(idx)" />
     </div>
   </div>
 </template>
@@ -14,7 +24,18 @@ export default {
   data() {
     return {
       Members: Members,
+      modalShow: false,
+      selectedIdx: -1,
     };
+  },
+  methods: {
+    openProfile(idx) {
+      this.selectedIdx = idx
+      this.modalShow = true
+    },
+    closeModal(){
+      this.modalShow = false
+    }
   },
 };
 </script>
@@ -22,12 +43,25 @@ export default {
 <style>
 .post {
   width: 100%;
-  height:100%;
+  height: 100%;
   display: grid;
   grid-template-columns: repeat(5, 1fr);
 }
-
 .post-content {
-  padding:10px;
+  padding: 10px;
+}
+
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+}
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
 }
 </style>
